827be2b0fa224ace85af917f126621530b98e6ec
Dan <danielgrenell@gmail.com>
2020-11-22 11:25:32 -0500

    Cleaned up word_count.py

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -3,11 +3,8 @@ from collections import Counter
 
 def count_words(sentence):
     chars = ':,."_\n\t!&@$%^'
-
     translator = str.maketrans(chars, ' '*len(chars))
-    sentence = ' '.join(word.translate(translator)
-                        for word in sentence.split())
-
-    sentence = (word.strip("'").lower() for word in sentence.split())
+    sentence = sentence.translate(translator).lower()
+    sentence = (word.strip("'") for word in sentence.split())
 
     return Counter(sentence)
